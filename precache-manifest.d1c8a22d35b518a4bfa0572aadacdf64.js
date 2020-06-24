e24f80a10023e869b128f678ca50dc3df582a0f0
Jesper Vang <jvang@jespers-mbp.lan>
2020-06-24 13:36:36 +0200

    Updates

---
+++ precache-manifest.d1c8a22d35b518a4bfa0572aadacdf64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6808e43cb44812e9d44e8c9117ec5409",
+    "url": "/react_application_test/index.html"
+  },
+  {
+    "revision": "984e90336af7dbbdb4f3",
+    "url": "/react_application_test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22dea648c700da722f6a",
+    "url": "/react_application_test/static/js/2.e163ebb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_application_test/static/js/2.e163ebb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984e90336af7dbbdb4f3",
+    "url": "/react_application_test/static/js/main.54d3b7e3.chunk.js"
+  },
+  {
+    "revision": "3b2a9cd57816ff8cc29d",
+    "url": "/react_application_test/static/js/runtime-main.0085f9e1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_application_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
